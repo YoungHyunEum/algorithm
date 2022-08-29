2649db4418cc0ef15c818eb597a3f10373486f30
Eum YoungHyun <100982636+YoungHyunEum@users.noreply.github.com>
2022-08-29 18:07:42 +0900

    Create 11279 최대 힙.cpp

---
+++ 백준/Silver/11279 최대 힙.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int N;
+priority_queue<int> pq;
+vector<int> v;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		int num;
+		cin >> num;
+
+		if (num == 0) {
+			if (pq.empty()) v.push_back(0);
+			else {
+				v.push_back(pq.top());
+				pq.pop();
+			}
+		}
+		else {
+			pq.push(num);
+		}
+	}
+
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << "\n";
+	}
+
+	return 0;
+}
