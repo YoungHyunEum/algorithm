a77555bbbbf949757c03e21ec4d53f246989e6cc
Eum YoungHyun <100982636+YoungHyunEum@users.noreply.github.com>
2022-09-01 23:29:23 +0900

    Create Baekjoon_1074.cpp

---
+++ 백준/Silver/Baekjoon_1074.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int n, r, c;
+int ans;
+
+void Z(int y, int x, int size)
+{
+    if (y == r && x == c)
+    {
+        cout << ans;
+        return;
+    }
+
+    if (r < y + size && r >= y && c < x + size && c >= x)
+    {
+        Z(y, x, size / 2);
+        Z(y, x + size / 2, size / 2);
+        Z(y + size / 2, x, size / 2);
+        Z(y + size / 2, x + size / 2, size / 2);
+    }
+    else
+    {
+        ans += size * size;
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n >> r >> c;
+    Z(0, 0, pow(2,n));
+    return 0;
+}
